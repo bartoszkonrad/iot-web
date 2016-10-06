b7973603cc4dc9ee8b9d718ee409a06b0ac01e51
bartoszkonrad <bartosz.konrad@gmail.com>
2016-10-06 16:04:56 +0200

    add login functionality; purecss; some improvements

--- iot.wsgi
+++ iot.wsgi
@@ -1,3 +1,4 @@
 import sys
 sys.path.insert(0, '/var/www/iot/')
 from iot import app as application
+application.secret_key = 'Yeeh0iah'
--- iot/__init__.py
+++ iot/__init__.py
@@ -1,44 +1,119 @@
 #!/usr/bin/python3
 # coding: utf-8
-# Bartosz Konrad <bartosz.konrad@gmail.com>
+# Bartosz Konrad <bartosz.konrad@talex.pl>
 
+import sys
 import datetime
-from flask import Flask
+# import logging
+from flask import Flask, request, redirect, url_for, abort, send_file, Response, render_template
+from flask_login import LoginManager, UserMixin, login_required, login_user, current_user, logout_user
 from flask_mysqldb import MySQL
-from flask import render_template
+
+# logger = logging.getLogger(__name__)
+# logger.setLevel(logging.INFO)
+# handler = logging.FileHandler('/var/www/html/iot/iot/iot.log')
+# handler.setLevel(logging.INFO)
+# formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# handler.setFormatter(formatter)
+# logger.addHandler(handler)
 
 app = Flask(__name__)
-
 mysql = MySQL(app)
-
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'zai1OoRo'
 app.config['MYSQL_DB'] = 'iot'
 app.config['MYSQL_HOST'] = 'localhost'
+login_manager = LoginManager()
+login_manager.init_app(app)
 
-@app.route("/")
-def hello():
+users = {'test': {'passwd': 'test'}, 'bartek': {'passwd': 'test'}}
+
+
+class User(UserMixin):
+    pass
+
+
+@login_manager.user_loader
+def user_loader(username):
+    if username not in users:
+        return
+    user = User()
+    user.id = username
+    return user
+
+
+@login_manager.request_loader
+def request_loader(request):
+    username = request.form.get('username')
+    if username not in users:
+        return
+    user = User()
+    user.id = username
+    user.is_authenticated = request.form['passwd'] == users[username]['passwd']
+    return user
+
+
+@app.route('/')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        if current_user.is_authenticated:
+            return redirect(url_for('dashboard'))
+        return render_template('login.html', text='homecontrol login')
+    try:
+        username = request.form['username']
+        if request.form['passwd'] == users[username]['passwd']:
+            checked = 'remember' in request.form
+            checked = True  # force remember
+            user = User()
+            user.id = username
+            login_user(user, remember=checked)
+            return redirect(url_for('dashboard'))
+    except KeyError:
+        return render_template('login.html', text='unauthorized')
+        # return redirect(url_for('login'))
+    return redirect(url_for('login'))
+
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
+
+
+@app.route('/lighting')
+@login_required
+def lighting():
+    return render_template('lighting.html')
+
+
+@app.route('/temperature')
+@login_required
+def temperature():
     sql = '''select * from sensors order by ts desc limit 1'''
     cur = mysql.connection.cursor()
     cur.execute(sql)
     rv = cur.fetchall()
-    return str(rv)
+    return render_template('temperature.html', text=str(rv))
+    # return 'Temperature; Logged in as: ' + current_user.id
 
-@app.route('/rgb')
-@app.route('/rgb/<room>')
-def rgb(room=None):
-    return render_template('rgb.html', room=room)
-
-@app.route('/colorpicker')
-@app.route('/colorpicker/<color>')
-def colorpicker(color=None):
-    return render_template('colorpicker.html', color=color)
 
 @app.route('/ws')
+@login_required
 def ws():
     return render_template('ws.html')
 
 
+@app.route('/logout')
+def logout():
+    logout_user()
+    return render_template('login.html', text='logged out')
+
+
+@login_manager.unauthorized_handler
+def unauthorized_handler():
+    # return 'Unauthorized'
+    return render_template('login.html', text='unauthorized')
+
 if __name__ == "__main__":
     app.run(debug=True)
-
